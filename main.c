9e94d5ec22ec43535d0a4791c552345ad4dfd292
Krishna <krishnamurugan589@gmail.com>
2024-04-29 15:13:58 +0530

    change color on bounce

--- main.c
+++ main.c
@@ -1,4 +1,15 @@
 #include "raylib.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+Color pickRandomColor() {
+    Color colorsArray[5] = {WHITE, GREEN, RED, BLUE, YELLOW};
+
+    srand(time(NULL));
+    int randomIndex = rand() % 5;
+    return colorsArray[randomIndex];
+}
 
 int main(void) {
     InitWindow(1280, 720, "Bounce");
@@ -10,18 +21,26 @@ int main(void) {
     int velocity_x = 2;
     int velocity_y = 2;
 
+    Color squareColor;
+
     while(!WindowShouldClose()) {
 
         BeginDrawing();
             ClearBackground(BLACK);
 
-            if (rect_x < 0 || rect_x > 1280 - 100) velocity_x *= -1;
-            if (rect_y < 0 || rect_y > 720 - 100) velocity_y *= -1;
+            if (rect_x < 0 || rect_x > 1280 - 100) {
+                velocity_x *= -1;
+                squareColor = pickRandomColor();
+            }
+            if (rect_y < 0 || rect_y > 720 - 100) {
+                velocity_y *= -1;
+                squareColor = pickRandomColor();
+            }
             
             rect_x += velocity_x;
             rect_y += velocity_y;
 
-            DrawRectangle(rect_x, rect_y, 100, 100, WHITE);
+            DrawRectangle(rect_x, rect_y, 100, 100, squareColor);
         EndDrawing();
 
     }
